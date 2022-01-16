178abc190ea96288f8007261b96561e932064514
Karan Agarwal <karanyobro@gmail.com>
2022-01-16 20:26:33 +0530

    program

---
+++ functions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+void repeat()
+{
+    cout<<"Hello I'm KKA";
+}
+
+int main()
+{
+    repeat();
+    repeat();
+    repeat();
+    repeat();
+    return 0;
+}
