f93afca8a5bfaa538a164c54d792178545a20fc0
Karan Agarwal (KKA) <karanyobro@gmail.com>
2023-01-10 01:17:17 +0530

    @

---
+++ OOPS/Patterns/right trinagle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=5,b,c,d;
+	for(b=0; a>b; b++)
+	{
+		for(c=0; b>c; c++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+//   
---
+++ OOPS/Patterns/square.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=5,b=5,c,d;
+	for(c=0; a>c; c++)
+	{
+		for(d=0; a>d; d++){
+			cout<<"* ";	
+		}
+		cout<<endl;
+	}
+	return 0;
+	
+}
