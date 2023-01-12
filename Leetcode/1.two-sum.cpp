3e28ba824ae41c20e70789a14179fa0a92ef61e9
Karan Agarwal (KKA) <karanyobro@gmail.com>
2023-01-12 14:01:27 +0530

    @

---
+++ Leetcode/1.two-sum.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
