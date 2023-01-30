8a5452e31274d1f1042161eba44973984608e672
Karan Agarwal (KKA) <karanyobro@gmail.com>
2023-01-30 16:11:36 +0530

    @

---
+++ Projects/FD-Calculator.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    float a,b,c;
+    char d;
+
+    cout<<"Enter the Money You want to Deposit (Example: 1000.0)"<<endl;
+    cin>>a;
+    cout<<"Enter The Percentage (Example: 3.25)"<<endl;
+    cin>>b;
+    cout<<"Enter the Months you Want to Deposit (Example: 1)"<<endl;
+    cin>>c;
+    cout<<endl<<"---------------------------------"<<endl;
+    cout<<"-------------Confirm:-------------";
+    cout<<endl<<"---------------------------------"<<endl;
+    cout<<endl<<"     Money: Rs "<<a<<endl<<"     Percentage: "<<b<<"%"<<endl<<"     Time: "<<c<<" Months";
+    cout<<endl<<"---------------------------------";
+    b = b/100;
+    c = c/12;
+    cout<<endl<<"  [Y/N]: ";
+    cin>>d;
+    cout<<"---------------------------------";
+    if (d == 'Y' || d == 'y')
+    {
+        int t = a*b*c;
+        cout<<endl<<"Your Total Interest is: "<<t<<endl;
+        cout<<"Total Monthly Interest is: "<<a*b*0.08<<endl;
+        cout<<"Total Money Received is: "<<t+a<<endl;
+        cout<<"---------------------------------";
+    }
+    
+}
